c549c5dd8ccdc15ae60a7833a093805c031b835e
Yehong Piao <piaoyehong1107@gmail.com>
2020-07-01 18:16:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = { }
+  attributes = { }
+  pigeon_name =[ ]
+  data.each { |key, value|
+    attributes.store(key, [])
+      value.each { |inner_key,inner_value|
+      inner_value.each {|name|
+         pigeon_name.push(name)
+          pigeon_list.store(name,attributes)
+        }
+      if inner_value.include? pigeon_name
+      attributes[key].push(inner_key.to_s)
+    end
+      }
+    }
+pigeon_name
+
 end
+nyc_pigeon_organizer(pigeon_data)
