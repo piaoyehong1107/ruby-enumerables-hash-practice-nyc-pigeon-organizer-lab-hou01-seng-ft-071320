6ac9d894c985667d3d69ae2ff00e61eef0c77dae
Yehong Piao <piaoyehong1107@gmail.com>
2020-07-01 20:27:20 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@ def nyc_pigeon_organizer(data)
     value.each do |inner_key,names|
         names.each do |name|
             if !final_array[name]
-                final_array[name][key] ={}
+                final_array[name] ={}
             end
             if !final_array[name][key]
                 !final_array[name][key] =[]
